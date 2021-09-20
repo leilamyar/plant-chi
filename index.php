2bdf7656487a39820d3bae26426f05b78c1f7806
leilamyar <leilamyar.dev@gmail.com>
2021-09-20 09:22:41 +0200

    wip

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php 
   include("views/html/head.php");  
   // include("controllers/router.php");
-  include("views/html/footer.php");
+  include("views/pages/login-play.php");
+  // include("views/html/footer.php");
 ?>
---
+++ views/pages/login-play.php
@@ -0,0 +1,22 @@
+<div class="flex-col-centered">
+  <h1 class="aligned-center">Plant chi</h1>
+  <form
+    action="#"
+    method="post">
+    <h2 class="aligned-center">SIGN UP</h2>
+    <div class="aligned-center">
+      <input type="email" name="user-email" id="user-email" placeholder="email">
+    </div>
+    <div class="aligned-center">
+      <input type="password" name="user-pwd" id="user-pwd" placeholder="password">
+    </div>
+    <div class="btn-custom">
+      <button type="submit">SIGN UP
+      </button>
+    </div>
+  </form>
+  
+  <div class="aligned-center">Already have an account? <a href="?=section=signin">Sign in</a></div>
+  
+  <div class="btn-custom">PLAY</div>
+</div>
