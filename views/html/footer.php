443e11216ccbefcdaa0b3836dc149eff5fcd4f23
Barbara <barbara.branco.c@gmail.com>
2021-09-21 20:58:26 +0200

    added colors var

--- views/html/footer.php
+++ views/html/footer.php
@@ -1,3 +1,8 @@
+<img src="public/images/bird_intro.png" alt="sweet little bird">
+<div class="container">
+  
+</div>
+
 <footer>
   <div rel="author" id="copyright">Created with &hearts; by Barbara, Nisrine and Leila.
   </div>
