68c55ef1a6d4bbc4c885371326c8d9ddb1916a35
leilamyar <leilamyar.dev@gmail.com>
2021-09-28 14:36:43 +0200

    add(spot) : img + dedicated css file

--- views/page/choose-a-spot.php
+++ views/page/choose-a-spot.php
@@ -1,14 +1,16 @@
 <main class="flex-c-col">
   <!-- COLUMN 1 = plant-chi img + text -->
-  <!-- <img src="" alt="Plant-chi gives instructions"> -->
-  <p>Choose the perfect spot for me <3 </p>
+  <div>
+    <div class="scale-img">
+      <img src="public/images/plant-chi_dead.png" alt="Plant-chi gives instructions">
+    </div>
+    <p>Choose the perfect spot for me</p>
+  </div>
   <!-- COLUMN 2 = room img -->
   <!-- COLUMN 2 = grid ON TOP of room img -->
-  <div class="choose-spot-grid">
-    <img src="" alt="A room with 3 possible spots for Plant-chi">
-  </div>
-  <div class="choose-spot-grid">
-    
+  <div class="choose-spot-game">
+    <!-- <img class="scale-img" src="public/images/rooms/bright_no_direct_sun.png" alt="Room with 3 possible spots for Plant-chi"> -->
+    <div class="choose-spot-grid"></div>
   </div>
 </main>
 <script src="public/js/choose-a-spot.js"></script>
