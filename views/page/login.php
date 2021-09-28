b22a6e00b1a9cad2dda58d7c5d6dcdabc708e1e7
leilamyar <leilamyar.dev@gmail.com>
2021-09-28 11:08:44 +0200

    refactor(login)

--- views/page/login.php
+++ views/page/login.php
@@ -1,6 +1,21 @@
- <p>I am login</p>
-
-
-
+<div class="flex-col-centered">
+  <h1 class="aligned-center">Plant chi</h1>
+  <form
+    action="#"
+    method="post">
+    <h2 class="aligned-center">SIGN UP</h2>
+    <div class="aligned-center">
+      <input type="email" name="user-email" id="user-email" placeholder="email">
+    </div>
+    <div class="aligned-center">
+      <input type="password" name="user-pwd" id="user-pwd" placeholder="password">
+    </div>
+      <button type="submit">SIGN UP
+      </button>
+  </form>
+  
+  <div class="aligned-center">Already have an account? <a href="?section=signin">Sign in</a></div>
+  
+  <a href="?section=play" class="btn">PLAY</a>
 
  <script src="public/js/login.js"></script>
