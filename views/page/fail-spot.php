14f2cc023a6763143955a8abf64470c829b3b566
nisrine <nisrinemr1@gmail.com>
2021-09-28 14:52:40 +0200

    fail page done.

--- views/page/fail-spot.php
+++ views/page/fail-spot.php
@@ -1,10 +1,18 @@
 <div class="fail-block">
-        <h1 class="fail-tittle">Dude, you killed me</h1>
+    <h1 class="fail-tittle">Dude, you killed me.</h1>
 
-        <img class="plant-chi-dead-img" src="public/images/plant-chi_dead.png" alt="dead plant-chi">
+    <img class="plant-chi-dead-img" src="public/images/plant-chi_dead.png" alt="dead plant-chi">
 
-        <br><input class="button-fail-spot-try-again" type="button" value="TRY AGAIN">
-    </div>
+    <br>
+    
+    <a href="?section=choose-a-room" class="btn">TRY AGAIN</a>
+    <br><br>
+
+    <h4>Little tips :D</h4>
+    <p class="text-tips">The Aloe Vera doesn't need a place where is too much light: Or the plant will die. <br> 
+    Please, choose a better place where is enoughf of brightness for the plant. Not too much, or the plant wil not live as much as the plant could.</p>
+    <p>By the way, if you hover your mouse on the proposals , you will see Plant-Chi's reaction! :D</p>
+</div>
 
 
 <script src="public/js/fail-spot.js"></script>
