4b3360e23e32622b1c41b4debf6a93853aaea612
nisrine <nisrinemr1@gmail.com>
2021-09-28 13:11:03 +0200

    first commit for the fail page. Not done yet.

--- views/page/fail-spot.php
+++ views/page/fail-spot.php
@@ -1,3 +1,10 @@
-<p>I am fail spot</p>
+<div class="fail-block">
+        <h1 class="fail-tittle">Dude, you killed me</h1>
+
+        <img class="plant-chi-dead-img" src="public/images/plant-chi_dead.png" alt="dead plant-chi">
+
+        <br><input class="button-fail-spot-try-again" type="button" value="TRY AGAIN">
+    </div>
+
 
 <script src="public/js/fail-spot.js"></script>
