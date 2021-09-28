a280200d5dc114b8fd58c9ad66543607c0aa80e5
leilamyar <leilamyar.dev@gmail.com>
2021-09-28 11:11:14 +0200

    fix(assets) : rm bird img from 1st page

--- views/html/footer.php
+++ views/html/footer.php
@@ -1,5 +1,3 @@
-<img src="public/images/bird_intro.png" alt="sweet little bird">
-<p>a paragraph to test the font</p>
 
 <footer>
   <div rel="author" id="copyright">Created with &hearts; by Barbara, Nisrine and Leila.
