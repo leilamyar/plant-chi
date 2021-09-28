aad6001157324eed918e412f1d5f45413f84ca39
leilamyar <leilamyar.dev@gmail.com>
2021-09-28 12:35:59 +0200

    add(home) : linked login page to home play btn

--- views/html/footer.php
+++ views/html/footer.php
@@ -1,9 +1,3 @@
 
-<footer>
-  <div rel="author" id="copyright">Created with &hearts; by Barbara, Nisrine and Leila.
-  </div>
-</footer>
-
-<!-- <script src="public/js/script.js"></script> --> 
 </body>
 </html>
--- views/page/login.php
+++ views/page/login.php
@@ -1,21 +1,31 @@
-<div class="flex-col-centered">
-  <h1 class="aligned-center">Plant chi</h1>
-  <form
-    action="#"
-    method="post">
-    <h2 class="aligned-center">SIGN UP</h2>
-    <div class="aligned-center">
-      <input type="email" name="user-email" id="user-email" placeholder="email">
-    </div>
-    <div class="aligned-center">
-      <input type="password" name="user-pwd" id="user-pwd" placeholder="password">
-    </div>
-      <button type="submit">SIGN UP
-      </button>
-  </form>
-  
-  <div class="aligned-center">Already have an account? <a href="?section=signin">Sign in</a></div>
-  
-  <a href="?section=play" class="btn">PLAY</a>
+<main class="login-c">
 
- <script src="public/js/login.js"></script>
+  <div class="flex-col-centered">
+    <div class="logo">
+      <img src="public/images/LOGO.png" alt="Plant-chi Logo">
+    </div>
+    <form
+      action="#"
+      method="post">
+      <h2 class="title">SIGN UP</h2>
+      <div class="aligned-center">
+        <input type="email" name="user-email" id="user-email" placeholder="email">
+      </div>
+      <div class="aligned-center">
+        <input type="password" name="user-pwd" id="user-pwd" placeholder="password">
+      </div>
+      <button type="submit">SIGN UP</button>
+    </form>
+
+    <div class="aligned-center">Already have an account? <a href="?section=signin">Sign in</a></div>
+
+    <a href="?section=play" class="btn">PLAY</a>
+
+    <script src="public/js/login.js"></script>
+
+  </div>
+  <footer>
+    <div rel="author" id="copyright">Created with &hearts; by Barbara, Nisrine and Leila.</div>
+  </footer>
+
+</main>
