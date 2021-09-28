cde735be2d09f6782f49e1a64018a2cb37914057
Barbara <barbara.branco.c@gmail.com>
2021-09-28 15:58:19 +0200

    added grid

--- views/page/choose-a-room.php
+++ views/page/choose-a-room.php
@@ -1,3 +1,12 @@
 <p>I am choose a room</p>
+<header>
+    <nav></nav>
+</header>
+<div class="grid-container">
+    <div class="grid-item">Room1</div>
+    <div class="grid-item">Room2</div>
+    <div class="grid-item">Room3</div>
+    <div class="grid-item">Room4</div>
+</div>
 
 <script src="public/js/choose-a-room.js"></script>
