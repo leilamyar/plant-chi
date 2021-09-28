5224568c7226a840bcd0fcffb8f109c35837c3e7
leilamyar <leilamyar.dev@gmail.com>
2021-09-28 11:00:51 +0200

    wip flex-c

--- views/page/choose-a-spot.php
+++ views/page/choose-a-spot.php
@@ -1,3 +1,11 @@
-<p>I am choose a spot</p>
-
+<main class="flex-c-col">
+  <!-- COLUMN 1 = plant-chi img + text -->
+  <!-- <img src="" alt="Plant-chi gives instructions"> -->
+  <p>Choose the perfect spot for me <3 </p>
+  <!-- COLUMN 2 = room img -->
+  <!-- COLUMN 2 = grid ON TOP of room img -->
+  <div class="choose-spot-grid">
+    
+  </div>
+</main>
 <script src="public/js/choose-a-spot.js"></script>
