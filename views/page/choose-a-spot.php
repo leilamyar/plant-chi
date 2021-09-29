69570c873285277aa888b852767c0a956bbcace1
leilamyar <leilamyar.dev@gmail.com>
2021-09-29 09:25:23 +0200

    wip

--- views/page/choose-a-spot.php
+++ views/page/choose-a-spot.php
@@ -1,25 +1,14 @@
 <main class="spot-choice-c">
-  <!-- <section class="flex-c-col"> -->
-    <!-- COLUMN 1 = plant-chi img + text -->
-    <div class="flex-c-row flex-aligned-left">
-        <img class="img-plantchi-head" src="public/images/plant-chi_dead.png" alt="Plant-chi gives instructions">
-      <p class="plantchi-speaks-p">Choose the perfect spot for me</p>
-    </div>
-  
-    <!-- START COLUMN 2 -->
-    <div class="spot-choice-c">
-  
-    <!-- COLUMN 2 = room img -->
-      <div class="room-spot-choice">
-        <img class="spot-choice-img"
-          src="public/images/rooms/bright_no_direct_sun.png" alt="Room with 3 possible spots for Plant-chi">
-      </div>
-  
-    <!-- COLUMN 2 = grid ON TOP of room img -->
-        <!-- <div class="choose-spot-grid"></div> -->
-  
-    <!-- END COLUMN 2 -->
-      </div>
-  <!-- </section> -->
+  <!-- COLUMN 1 = plant-chi img + text -->
+  <div class="instructions-c">
+    <!-- <div class="img-c"> -->
+      <img class="plant-chi-img" src="public/images/plant-chi_neutral.png" alt="Plant-chi gives instructions">
+    <!-- </div> -->
+    <p>Choose the right spot for me</p>
+  </div>
+  <!-- COLUMN 2 = room img -->
+     
+  <!-- COLUMN 2 = grid ON TOP of room img -->
+
 </main>
 <script src="public/js/choose-a-spot.js"></script>
