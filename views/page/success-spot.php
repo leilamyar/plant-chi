6a2b8e9cf1ced8abed329ed326406b6e91b43834
leilamyar <leilamyar.dev@gmail.com>
2021-09-29 15:15:59 +0200

    feat(success) : style ground

--- views/page/success-spot.php
+++ views/page/success-spot.php
@@ -1,1 +1,10 @@
-<div>That place is perfect ! Thanks ! </div>
+<!-- <p>That spot is peeerfect!
+<br>
+This room is bright
+and I’m far from the
+window...
+<br>
+I feel good here,
+thanks !</p> -->
+
+<div class="ground">&nbsp;</div>
