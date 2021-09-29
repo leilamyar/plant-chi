43c28db719d892d72dceb4e9f293b7c37353d431
leilamyar <leilamyar.dev@gmail.com>
2021-09-29 11:14:58 +0200

    feat(spot-choice) : style good spot

--- views/page/choose-a-spot.php
+++ views/page/choose-a-spot.php
@@ -1,9 +1,7 @@
 <main class="spot-choice-c">
   <!-- COLUMN 1 = plant-chi img + text -->
   <div class="instructions-flex-c">
-    <!-- <div class="img-c"> -->
       <img class="plant-chi-img" src="public/images/plant-chi_neutral.png" alt="Plant-chi gives instructions">
-    <!-- </div> -->
     <p class="text">Choose the right spot for me &#60;3 </p>
   </div>
   <!-- COLUMN 2 = room img -->
@@ -11,6 +9,11 @@
     <img class="room-img" src="public/images/rooms/bright_no_direct_sun.png" alt="Image of a bright room, with no direct sunlight">
   </div>
   <!-- COLUMN 2 = grid ON TOP of room img -->
-
+  <div class="room-c">
+    <!-- <div class="room-img"></div> -->
+    <div class="spot" id="good-spot">&nbsp;</div>
+    <div class="spot" id="wrong-spot">&nbsp;</div>
+    <div class="spot" id="wrong-spot">&nbsp;</div>
+  </div>
 </main>
 <script src="public/js/choose-a-spot.js"></script>
