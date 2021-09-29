1d24d2cdf1a6f764c42124f64e3b33029665910d
nisrine <nisrinemr1@gmail.com>
2021-09-29 10:18:38 +0200

    added article but problem with css

--- views/page/choose-a-plant.php
+++ views/page/choose-a-plant.php
@@ -1,3 +1,35 @@
+
+<section>
+    <h1 class="tittle-choose-plant"> Choose your plant &#10084; </h1>
+    
+    <article>
+        <h2 class="tittle-two-choose-plant"> Tutorial: Aloe Vera</h2>
+        <img class="img-choose-plant" src="public/images/bird_intro.png" alt="Aloe Vera">
+    </article>
+
+
+    <article>
+        <h2 class="tittle-two-choose-plant"> lvl 1 - locked</h2>
+        <img class="img-choose-plant" src="public/images/bird_intro.png" alt="Aloe Vera">
+    </article>
+
+
+    <article>
+        <h2> lvl 2 - locked</h2>
+        <p>...</p>
+    </article>
+
+
+    <article>
+        <h2> lvl 3 - locked</h2>
+        <p>...</p>
+    </article>
+
+</section>
+ 
+ 
+ 
+ 
  <!-- +message when the user choosed the plant! --> 
  <p>I am choose a plant</p>
 
