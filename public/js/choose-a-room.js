034a4097804722d0528671974acd448e3802ea4c
Barbara <barbara.branco.c@gmail.com>
2021-10-05 16:09:17 +0200

    wrongRoom

--- public/js/choose-a-room.js
+++ public/js/choose-a-room.js
@@ -1,37 +1,48 @@
-/* console.log("Hello Choose a room");
+console.log("Hello Choose a room");
 
+/* document.getElementById("wrongRoom").addEventListener("mouseover", mouseOver);
+document.getElementById("wrongRoom").addEventListener("mouseout", mouseOut);
 
-const wrongRoom = document.querySelectorAll("#wrong-room-1");
+function mouseOver() {
+  document.getElementById("wrongRoom").;
+}
+
+function mouseOut() {
+  document.getElementById("demo").style.color = "black";
+} */
+
+const wrongRoom = document.querySelector("#wrong-room-1");
 const goodRoom = document.querySelector("#good-room");
-const rooms = document.querySelectorAll(".spot");
-const textEl = document.querySelector('.text');
+const roomsArray = document.querySelectorAll(".grid-item");
+const textEl = document.querySelector('.text-plant');
 
-spots.forEach((room) => {
-  spot.addEventListener('mouseleave', () => {
-    const plantchiImg = document.getElementById('plant-chi-img');
+
+
+roomsArray.forEach((room) => {
+  room.addEventListener('mouseleave', () => {
+    const plantchiImg = document.getElementById('plant-chi_neutral');
     plantchiImg.remove();
-    const imgEl = document.('img');
+    const imgEl = document.createElement('img');//<img>
     imgEl.src = 'public/images/plant-chi/plant-chi_neutral.png';
-    imgEl.setAttribute('id', 'plant-chi-img');
+    imgEl.setAttribute('id', 'plant-chi_neutral');
     textEl.parentNode.insertBefore(imgEl, textEl);
   });
 });
 
-wrongRoom.forEach((wrongRoom) => {
-  wrongRoom.addEventListener('mouseover', () => {
-    const plantchiImg = document.getElementById('room.png');
-    plantchiImg.remove();
-    const imgEl = document.createElement('img');
+wrongRoom.addEventListener('mouseover', () => {
+    const plantchiImgNeutral = document.getElementById('plant-chi_neutral');
+    plantchiImgNeutral.remove();
+    const imgEl = document.createElement('img');//<img>
     imgEl.src = 'public/images/plant-chi/plant-chi_unhappy.png';
     imgEl.setAttribute('id', 'plant-chi-img');
     textEl.parentNode.insertBefore(imgEl, textEl);
-  });
 });
-goodRoom.addEventListener('mouseover', () => {
-  const plantchiImg = document.getElementById('plant-chi-img');
+
+/* goodRoom.addEventListener('mouseover', () => {
+  const plantchiImg = document.getElementById('righRoom');
   plantchiImg.remove();
   const imgEl = document.createElement('img');
   imgEl.src = 'public/images/plant-chi/plant-chi_happy.png';
   imgEl.setAttribute('id', 'plant-chi-img');
   textEl.parentNode.insertBefore(imgEl, textEl);
-}); */
+});  */
--- views/page/choose-a-room.php
+++ views/page/choose-a-room.php
@@ -1,16 +1,17 @@
 <p>I am choose a room</p>
 <div class="header">
     <div class="inner-header">
-        <div class="logo-container"><img src="public/images/plantchi_happy.png" alt="plantchi_happy">
-        <h1 class="text-plant">Choose the perfect room for me</h1>
+        <div class="logo-container">
+            <img id="plant-chi_neutral" src="public/images/plant-chi/plant-chi_neutral.png" alt="plantchi_neutral">
+            <h1 class="text-plant">Choose the perfect room for me</h1>
         </div>
     </div>
 </div>
 <br>
 <div class="grid-container">
-    <div class="grid-item"><img src="public/images/garden.png" alt="garden"></div>
-    <div class="grid-item"><img src="public/images/room.png" alt="room"></div>
-    <div class="grid-item">Room3</div>
+    <div class="grid-item" id="wrong-room-1"><img src="public/images/garden.png" alt="garden"></div>
+    <div class="grid-item" id="rightRoom"><img src="public/images/room.png" alt="room"></div>
+    <div class="grid-item" >Room3</div>
     <div class="grid-item">Room4</div>
 </div>
 
