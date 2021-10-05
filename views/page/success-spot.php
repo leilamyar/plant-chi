1e8ef793b02b2298edaba40a771ff96ea9d3261b
leilamyar <leilamyar.dev@gmail.com>
2021-10-05 14:42:20 +0200

    style(success) : center img and text in the middel

--- views/page/success-spot.php
+++ views/page/success-spot.php
@@ -1,13 +1,30 @@
-<div class="plantchi-speaks-flex-c">
-  <img id="plantchi-spot-img" src="public/images/plant-chi/plant-chi_happy.png" alt="Plant-chi is happy with the spot">
-  <p class="plantchi-text">That spot is peeerfect!
-    <br>
-    This room is bright
-    and I’m far from the
-    window...
-    <br>
-    I feel good here,
-    thanks !</p>
-  </div>
+<!-- <div class="xxx">
+  <div class="plantchi-speaks-flex-c vertically-centered">
+    <img id="plantchi-spot-img" src="public/images/plant-chi/plant-chi_happy.png" alt="Plant-chi is happy with the spot">
+    <p class="plantchi-text">That spot is peeerfect!
+      <br>
+      This room is bright and
+      <br>
+      I’m far from the
+      window...
+      <br>
+      I feel good here,
+      thanks !</p>
+    </div>
+</div>
+-->
 
-<div class="ground">&nbsp;</div>
+<div class="success-grid-c">
+  <!-- Plant-chi img -->
+  <div id="grid-item-1">
+    <img id="plantchi-success-img" src="public/images/plant-chi/plant-chi_happy.png" alt="Plant-chi is happy with the spot">
+  </div>
+  <!-- Plant-chi text (to the right of the img) -->
+  <div id="grid-item-2">
+    <p>That spot is peeerfect!</p>
+    <p>This room is bright and</p>
+    <p>I’m far from the window...</p>
+    <p>I feel good here, thanks !</p>
+  </div>
+</div>
+<!-- <div class="ground">&nbsp;</div> -->
