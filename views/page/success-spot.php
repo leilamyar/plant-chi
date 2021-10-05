783ed7cef97be9d27321547b77a689ee7aa5ec02
leilamyar <leilamyar.dev@gmail.com>
2021-10-05 09:13:31 +0200

    style(succes) : use style from spot choice

--- views/page/success-spot.php
+++ views/page/success-spot.php
@@ -1,10 +1,13 @@
-<!-- <p>That spot is peeerfect!
-<br>
-This room is bright
-and I’m far from the
-window...
-<br>
-I feel good here,
-thanks !</p> -->
+<div class="plantchi-speaks-flex-c">
+  <img id="plantchi-spot-img" src="public/images/plant-chi/plant-chi_happy.png" alt="Plant-chi is happy with the spot">
+  <p class="plantchi-text">That spot is peeerfect!
+    <br>
+    This room is bright
+    and I’m far from the
+    window...
+    <br>
+    I feel good here,
+    thanks !</p>
+  </div>
 
 <div class="ground">&nbsp;</div>
