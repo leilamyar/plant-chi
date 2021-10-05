35370cbad600be1a3328698d363d1746cf792da9
Barbara <barbara.branco.c@gmail.com>
2021-10-05 13:42:56 +0200

    name changing room file

--- public/js/choose-a-room.js
+++ public/js/choose-a-room.js
@@ -1,2 +1,37 @@
-console.log("Hello Choose a room");
-console.log("hi ");
+/* console.log("Hello Choose a room");
+
+
+const wrongRoom = document.querySelectorAll("#wrong-room-1");
+const goodRoom = document.querySelector("#good-room");
+const rooms = document.querySelectorAll(".spot");
+const textEl = document.querySelector('.text');
+
+spots.forEach((room) => {
+  spot.addEventListener('mouseleave', () => {
+    const plantchiImg = document.getElementById('plant-chi-img');
+    plantchiImg.remove();
+    const imgEl = document.('img');
+    imgEl.src = 'public/images/plant-chi/plant-chi_neutral.png';
+    imgEl.setAttribute('id', 'plant-chi-img');
+    textEl.parentNode.insertBefore(imgEl, textEl);
+  });
+});
+
+wrongRoom.forEach((wrongRoom) => {
+  wrongRoom.addEventListener('mouseover', () => {
+    const plantchiImg = document.getElementById('room.png');
+    plantchiImg.remove();
+    const imgEl = document.createElement('img');
+    imgEl.src = 'public/images/plant-chi/plant-chi_unhappy.png';
+    imgEl.setAttribute('id', 'plant-chi-img');
+    textEl.parentNode.insertBefore(imgEl, textEl);
+  });
+});
+goodRoom.addEventListener('mouseover', () => {
+  const plantchiImg = document.getElementById('plant-chi-img');
+  plantchiImg.remove();
+  const imgEl = document.createElement('img');
+  imgEl.src = 'public/images/plant-chi/plant-chi_happy.png';
+  imgEl.setAttribute('id', 'plant-chi-img');
+  textEl.parentNode.insertBefore(imgEl, textEl);
+}); */
--- views/page/choose-a-room.php
+++ views/page/choose-a-room.php
@@ -1,7 +1,12 @@
 <p>I am choose a room</p>
-<header>
-    <nav></nav>
-</header>
+<div class="header">
+    <div class="inner-header">
+        <div class="logo-container"><img src="public/images/plantchi_happy.png" alt="plantchi_happy">
+        <h1 class="text-plant">Choose the perfect room for me</h1>
+        </div>
+    </div>
+</div>
+<br>
 <div class="grid-container">
     <div class="grid-item"><img src="public/images/garden.png" alt="garden"></div>
     <div class="grid-item"><img src="public/images/room.png" alt="room"></div>
