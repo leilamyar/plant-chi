a5a6b766254a845f2edee0d7a402c73699ca3754
Barbara <barbara.branco.c@gmail.com>
2021-10-12 14:46:05 +0200

    text added

--- public/js/choose-a-room.js
+++ public/js/choose-a-room.js
@@ -10,29 +10,153 @@ const textEl = document.querySelector('.text-plant');
 
 roomsArray.forEach((room) => {
   room.addEventListener('mouseleave', () => {
-    const plantchiImg = document.getElementById('plant-chi_neutral');
-    plantchiImg.remove();
-    const imgEl = document.createElement('img');//<img>
+    const plantchiImg = document.getElementById('plantchiCommentContainer');
+    if(plantchiImg){
+      plantchiImg.remove();
+    }
+    
+    const neutralState = document.createElement('div');//div
+    neutralState.setAttribute('id', 'plantchiCommentContainer');
+    neutralState.setAttribute('class', 'logo-container');
+
+    const anchorComment = document.getElementById("anchorCommentContainer");
+    anchorComment.appendChild(neutralState);
+
+    const neutralStateImg = document.createElement('img');//<img>
+    neutralStateImg.src = 'public/images/plant-chi/plant-chi_neutral.png';
+    neutralStateImg.setAttribute('id', 'plant-chi_neutral');
+
+    
+
+    const neutralStateH1=document.createElement("h1");//<h1>
+    neutralStateH1.innerText = "Choose the right room for me."
+    neutralStateH1.setAttribute('class', 'text-plant');
+    neutralStateH1.setAttribute('id', 'textPlant');
+    //TODO Discover how to add text to H1 I just created
+    neutralState.appendChild(neutralStateImg);
+    neutralState.appendChild(neutralStateH1);
+  
+
+
+
+    /* textEl.parentNode.insertBefore(imgEl, textEl); */
+
+    //old way of adding image(not in container and won't stop)
+    /* const imgEl = document.createElement('img');//<img>
     imgEl.src = 'public/images/plant-chi/plant-chi_neutral.png';
     imgEl.setAttribute('id', 'plant-chi_neutral');
-    textEl.parentNode.insertBefore(imgEl, textEl);
+    textEl.parentNode.insertBefore(imgEl, textEl); */
+
+    
   });
+
 });
 
-wrongRoom.addEventListener('mouseover', () => {
-    const plantchiImgNeutral = document.getElementById('plant-chi_neutral');
-    plantchiImgNeutral.remove();
+
+
+wrongRoom.addEventListener('mouseenter', () => {
+
+    const plantchiImg = document.getElementById("plantchiCommentContainer");
+    //remove elements
+    if(plantchiImg){
+      plantchiImg.remove();
+    }
+
+    const neutralState = document.createElement('div');//div
+    neutralState.setAttribute('id', 'plantchiCommentContainer');
+    neutralState.setAttribute('class', 'logo-container');
+
+    const plantchiSadReplaceImg = document.createElement('img');
+    plantchiSadReplaceImg.src = "public/images/plant-chi/plant-chi_unhappy.png";
+    neutralState.appendChild(plantchiSadReplaceImg);
+
+    const plantchiSadReplace = document.createElement('h1');
+    plantchiSadReplace.innerText = "Not this one...Or I will Die!";
+    neutralState.appendChild(plantchiSadReplace);
+
+
+    const anchorComment = document.getElementById("anchorCommentContainer");
+    anchorComment.appendChild(neutralState);
+
+
+    
+    //old way of removing text
+
+    /* const plantchiTextSadGet = document.
+    getElementById('textPlant');
+    if(document.
+      getElementById('textPlant');)
+    plantchiTextSadGet.remove(); */ 
+
+    //replace 
+
+    
+    /* const plantchiTextSadReplace = document.createElement('h1');
+    const newNode=document.createTextNode("Not this one...Or I will Die!")
+    newNode.appendChild("logo-container");
+
+    
+    document.getElementById("logo-container").appendChild(newNode);
+
+    console.log(newNode);
+
     const imgEl = document.createElement('img');//<img>
+
     imgEl.src = 'public/images/plant-chi/plant-chi_unhappy.png';
-    imgEl.setAttribute('id', 'plant-chi-img');
-    textEl.parentNode.insertBefore(imgEl, textEl);
+    imgEl.setAttribute('id', 'plant-chi_neutral'); */
+
+    /* textEl.appendChild(imgEl, textEl); */
+
+  ////////////////////////////
+    /* const plantchiTextSadGet = document.
+    getElementById('textPlant');
+    plantchiTextSadGet.remove();
+    const plantchiTextSadReplace = document.createElement('h2');//<h2>
+    plantchiTextSadReplace.innerText = 'All Good';
+    plantchiTextSadReplace.setAttribute('id', 'plantchiTextSadReplace');
+    textEl.parentNode.insertBefore(plantchiTextSadReplace, textEl); */ 
+
 });
+
+
+
+//RightRoom
 
 rightRoom.addEventListener('mouseover', () => {
-  const plantchiImgNeutral2 = document.getElementById('plant-chi_neutral');
+  const plantchiImg = document.getElementById("plantchiCommentContainer");
+    //remove elements
+    if(plantchiImg){
+      plantchiImg.remove();
+    }
+
+    const neutralState = document.createElement('div');//div
+    neutralState.setAttribute('id', 'plantchiCommentContainer');
+    neutralState.setAttribute('class', 'logo-container');
+
+    const plantchiHappyReplaceImg = document.createElement('img');
+    plantchiHappyReplaceImg.src = "public/images/plant-chi/plant-chi_happy.png";
+    neutralState.appendChild(plantchiHappyReplaceImg);
+
+    const plantchiHappyReplace = document.createElement('h1');
+    plantchiHappyReplace.innerText = "This one looks perfect!";
+    neutralState.appendChild(plantchiHappyReplace);
+
+
+    const anchorComment = document.getElementById("anchorCommentContainer");
+    anchorComment.appendChild(neutralState);
+  /* const plantchiImgNeutral2 = document.getElementById('plant-chi_neutral');
   plantchiImgNeutral2.remove();
   const imgEl = document.createElement('img');
   imgEl.src = 'public/images/plant-chi/plant-chi_happy.png';
-  imgEl.setAttribute('id', 'plant-chi-img');
+  imgEl.setAttribute('id', 'plant-chi_neutral');
   textEl.parentNode.insertBefore(imgEl, textEl);
+///((////////////////////////))
+  const plantchiTextHappyGet = document.getElementById('plant-chi_neutral');
+  plantchiTextHappyGet.remove();
+  const plantchiTextHappyReplace = document.createElement('h2');
+  plantchiTextHappyReplace.innerText = 'Are we good or not?';
+  plantchiTextHappyReplace.setAttribute('id', 'plantchiTextHappyReplace');
+  textEl.parentNode.insertBefore(plantchiTextHappyReplace, textEl); */
+
+
 });  
--- views/page/choose-a-room.php
+++ views/page/choose-a-room.php
@@ -1,9 +1,10 @@
 <p>I am choose a room</p>
 <div class="header">
-    <div class="inner-header">
-        <div class="logo-container">
+    <div class="inner-header" id="anchorCommentContainer">
+        <div class="logo-container" id="plantchiCommentContainer">
             <img id="plant-chi_neutral" src="public/images/plant-chi/plant-chi_neutral.png" alt="plantchi_neutral">
-            <h1 class="text-plant">Choose the perfect room for me</h1>
+            <h1 class="text-plant" id="textPlant">Choose the perfect room for me</h1>
+
         </div>
     </div>
 </div>
@@ -11,7 +12,7 @@
 <div class="grid-container">
     <div class="grid-item" id="wrong-room-1"><img src="public/images/garden.png" alt="garden"></div>
     <div class="grid-item" id="rightRoom"><img src="public/images/room.png" alt="room"></div>
-    <div class="grid-item" >Room3</div>
+    <div class="grid-item">Room3</div>
     <div class="grid-item">Room4</div>
 </div>
 
