ac478c8b0f9c74dd9b593e65ce022e2719392b03
leilamyar <leilamyar.dev@gmail.com>
2021-10-14 16:46:22 +0200

    wip fix JS - css broken here

--- views/page/choose-a-spot.php
+++ views/page/choose-a-spot.php
@@ -1,7 +1,15 @@
 <main class="spot-choice-c">
   <!-- COLUMN 1 = plant-chi img + text -->
-  <div class="plantchi-speaks-flex-c">
-      <img id="plantchi-spot-img" src="public/images/plant-chi/plant-chi_neutral.png" alt="Plant-chi gives instructions">
+  <div id="plantchi-img-neutral" class="plantchi-speaks-flex-c">
+    <img src="public/images/plant-chi/plant-chi_neutral.png" alt="Plant-chi gives instructions">
+    <p class="plantchi-text">Choose the right spot for me &#60;3 </p>
+  </div>
+  <div id="plantchi-img-unhappy" class="plantchi-speaks-flex-c hidden">
+    <img src="public/images/plant-chi/plant-chi_unhappy.png" alt="Plant-chi is unhappy">
+    <p class="plantchi-text">Choose the right spot for me &#60;3 </p>
+  </div>
+  <div id="plantchi-img-happy" class="plantchi-speaks-flex-c hidden">
+    <img src="public/images/plant-chi/plant-chi_happy.png" alt="Plant-chi is happy">
     <p class="plantchi-text">Choose the right spot for me &#60;3 </p>
   </div>
   <!-- COLUMN 2 = room img -->
@@ -25,9 +33,7 @@
   <section class="kevin-body">
     <div class="kevin-card">
       <img class="kevin-card-image" src="public/images/rooms/bright_no_direct_sun.png" alt="">
-      <!-- <div> -->
-        <a class="spot" id="good-spot" href="?section=success-spot">&nbsp;</a>
-      <!-- </div> -->
+      <a href="?section=success-spot" class="spot" id="good-spot" >&nbsp;</a>
       <a href="?section=fail-spot" class="spot" id="wrong-spot-1">&nbsp; </a>
       <a href="?section=fail-spot" class="spot" id="wrong-spot-2">&nbsp;</a>
     </div>
