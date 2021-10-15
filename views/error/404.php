388a42f8a7433bcc1465ad70965694a6eba0f0e5
leilamyar <leilamyar.dev@gmail.com>
2021-10-15 16:42:14 +0200

    style(404)

--- views/error/404.php
+++ views/error/404.php
@@ -1,3 +1,7 @@
-<h2>ERROR 404</h2>
-<h2>Sorry, the page you asked doesn't exist.</h2>
-<div><a href="?section=home" class="btn">HOME</a></div>
+<div class="aligned-center">
+  <h2>ERROR 404</h2>
+  <p>Sorry, the page you asked doesn't exist.</p>
+  <div><a href="?section=home" class="btn">HOME</a></div>
+  <img class="peanut-img" src="public/images/peanut.png" alt="">
+</div>
+<link rel="stylesheet" href="public/css/404.css">
