350c9b6e36de074393f8231e5e60f1fd4631e9dd
Barbara <barbara.branco.c@gmail.com>
2021-10-15 14:31:32 +0200

    add/html

---
+++ views/page/choose-a-spot-cta.php
@@ -0,0 +1,10 @@
+<div class="plant-choosen">
+    <div class="plant-chi-speaks">
+        <a href="?section=choose-a-room">
+            <img class="img-plant-chi-presentation" src="public/images/plant-chi/plant-chi_neutral.png" alt="Plant-chi talk">
+        </a>
+        <a class="plant-chi-presentation" href="?section=choose-a-room">
+            <p>Hello {username}, I’m Aloe Vera :D<br><br>I’m so happy to have a new friend!<br>Let me tell you what i need to keep me alive :)</p>
+        </a>
+    </div>
+</div>
