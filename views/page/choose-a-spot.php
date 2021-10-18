9c3e7c1aa8ca2004046b0b619fe15c8d9b4af5e8
leilamyar <leilamyar.dev@gmail.com>
2021-10-18 11:05:51 +0200

    fix layout

--- views/page/choose-a-spot.php
+++ views/page/choose-a-spot.php
@@ -1,15 +1,15 @@
 <main class="spot-choice-c">
   <!-- COLUMN 1 = plant-chi img + text -->
   <div id="plantchi-img-neutral" class="plantchi-speaks-flex-c">
-    <img src="public/images/plant-chi/plant-chi_neutral.png" alt="Plant-chi gives instructions">
+    <img class="img-plantchi-spot" src="public/images/plant-chi/plant-chi_neutral.png" alt="Plant-chi gives instructions">
     <p class="plantchi-text">Choose the right spot for me &#60;3 </p>
   </div>
   <div id="plantchi-img-unhappy" class="plantchi-speaks-flex-c hidden">
-    <img src="public/images/plant-chi/plant-chi_unhappy.png" alt="Plant-chi is unhappy">
+    <img class="img-plantchi-spot" src="public/images/plant-chi/plant-chi_unhappy.png" alt="Plant-chi is unhappy">
     <p class="plantchi-text">Choose the right spot for me &#60;3 </p>
   </div>
   <div id="plantchi-img-happy" class="plantchi-speaks-flex-c hidden">
-    <img src="public/images/plant-chi/plant-chi_happy.png" alt="Plant-chi is happy">
+    <img class="img-plantchi-spot" src="public/images/plant-chi/plant-chi_happy.png" alt="Plant-chi is happy">
     <p class="plantchi-text">Choose the right spot for me &#60;3 </p>
   </div>
   <!-- COLUMN 2 = room img -->
