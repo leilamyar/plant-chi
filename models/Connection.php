ab803f7238e91b99284539a21111a8e468e5a352
nisrine <nisrinemr1@gmail.com>
2021-10-19 11:04:30 +0200

    comments added first commit

--- models/Connection.php
+++ models/Connection.php
@@ -2,17 +2,17 @@
 // abstrat pour ne pas pouvoir faire :
 // $c = new Connection()
 abstract class Connection {
-	private static $_connection;
-	public function __construct()
-	{
-		$this->dbConnection();
-	}
-
-	protected function dbConnection()
+    private static $_connection;
+    public function __construct()
     {
-    	if (self::$_connection !== null) {
-    		return self::$_connection;
-    	}
+        $this->dbConnection();
+    }
+
+    protected function dbConnection()
+    {
+        if (self::$_connection !== null) {
+            return self::$_connection;
+        }
         self::$_connection = new PDO("mysql:host=localhost;dbname=plantchi;charset=utf8", 'root', 'root');
         //to get the db of the sentences
         self::$_connection->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
@@ -24,17 +24,17 @@ abstract class Connection {
     protected function execute($query, $params= array(), $fetchMode = null)
     {
         // prépare la requete éviter injection SQL
-    	$stmt = self::$_connection->prepare($query);
+        $stmt = self::$_connection->prepare($query);
         // exucute la requte
-    	$stmt->execute($params);
+        $stmt->execute($params);
 
-    	if ($fetchMode !== null) {
+        if ($fetchMode !== null) {
             // retourne toutes les données sous forme de tableau
-    		return $stmt->fetchAll($fetchMode);
-    	} else {
-    		return $stmt->fetchAll();
-    	}
-    	return $stmt; // Iterator (forearch)
+            return $stmt->fetchAll($fetchMode);
+        } else {
+            return $stmt->fetchAll();
+        }
+        return $stmt; // Iterator (forearch)
     }
 }
  ?>
