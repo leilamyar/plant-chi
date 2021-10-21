a4cbf3aee07e0aa10c740de8dd809e86a4f52b58
Barbara <barbara.branco.c@gmail.com>
2021-10-21 16:11:20 +0200

    htmlTryWeather

--- views/page/amaterasu.php
+++ views/page/amaterasu.php
@@ -16,13 +16,22 @@
 
         </div>
         <div class="block-left">
-            <div id="meteo">
-                <p id="temperature"></p>
-                <img id="image" src="" alt="">
-            </div>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Icon</th>
+                        <th>Day of the week</th>
+                    </tr>
+                </thead>
+                <tbody id="myTable">
+
+                </tbody>
+            </table>
+
+            <button id="myButton">Call for Amaterasu</button>
         </div>
         <div class="block-right">
-            <button id="">Call for Amaterasu</button>
+
         </div>
 
         <!-- <table class="block-right">
@@ -53,5 +62,9 @@
 
 
     </div>
+    <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
     <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
     <script src="public/js/extra-points-amaterasu.js"></script>
+
+    <!-- <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+    <script src="public/js/extra-points-amaterasu.js"></script> -->
