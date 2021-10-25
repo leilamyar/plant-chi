5efb3a373197925e50fd044e1374a766023d2a71
nisrine <nisrinemr1@gmail.com>
2021-10-25 15:49:48 +0200

    deleted

--- views/page/logout.php
+++ views/page/logout.php
@@ -1,5 +1,5 @@
 <div class="aligned-center">
     <h4>Successfully logged out.</h4>
     <br>
-    <div><a href="?section=home">Back to Home page</a></div>
+    <div><a href="/plant-chi/">Back to Home page</a></div>
 </div>
