d622b73fc4c8999621564f46ea126215242a5029
Barbara <barbara.branco.c@gmail.com>
2021-10-27 19:23:50 +0200

    amaterasuAnim

--- views/page/amaterasu.php
+++ views/page/amaterasu.php
@@ -34,37 +34,17 @@
             <button id="myButton">Call for Amaterasu</button>
         </div>
 
-        <!-- <table class="block-right">
-            <tr>
-                <th>Icon</th>
-                <th>Day of the week</th>
-            </tr>
-            <tr>
-                <th><img src="public/images/test-meteo/ciel-voile.png" alt="ciel voile"></th>
-                <th>Monday</th>
-            </tr>
-
-            <tr>
-                <th><img src="public/images/test-meteo/eclaircies.png" alt="eclaircies"></th>
-                <th>Tuesday</th>
-            </tr>
-
-            <tr>
-                <th><img src="public/images/test-meteo/testeclaircies.png" alt="eclaircies"></th>
-                <th>Wednesday</th>
-            </tr>
-            <tr>
-                <th><img src="public/images/test-meteo/eclaircies.png" alt="eclaircies"></th>
-                <th>Thursday</th>
-            </tr>
-
-        </table> -->
+        <div class="jelly-container">
+            <div class="jelly-wrapper">
+                <div class="jelly-body">
+                    <img src="public/images/amaterasu_vertical.jpg" alt="amaterasu volante">
+                </div>
+            </div>
+            <div class="jelly-shadow"> </div>
+        </div>
 
 
     </div>
     <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
     <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
     <script src="public/js/extra-points-amaterasu.js"></script>
-
-    <!-- <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
-    <script src="public/js/extra-points-amaterasu.js"></script> -->
